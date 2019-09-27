1a22701705bcbb8314c94a6ba4efd929eef4f733
Sviatlana Kruhlikava <44160392+SviatlanKa@users.noreply.github.com>
2019-09-27 13:41:03 +0300

    finished project
    Finished project. Next step is FCC testing by CodePen

--- src/index.js
+++ src/index.js
@@ -4,13 +4,21 @@ import './style.css';
 const url = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json";
 
 const width = 1000;
-const height = width / 2;
+const height = width * 0.6;
 const margin = {
     left: width / 10,
     right: width / 40,
     top: height / 50,
-    bottom: height / 10
+    bottom: height / 20
 };
+const legendWidth = width / 2;
+const legendHeight = height / 10;
+
+let translateAxis = {
+    x: 0,
+    y: 0
+};
+
 const myColor = [
     '#a50026','#d73027','#f46d43','#fdae61','#fee090','#ffffbf',
     '#e0f3f8','#abd9e9','#74add1','#4575b4','#313695'
@@ -41,6 +49,21 @@ const tooltip = d3.select("#main")
 const chartPlot = chart.append("g")
     .attr("transform", `translate(${margin.left},${margin.top})`);
 
+chart.append("text")
+    .attr("text-anchor", "end")
+    .attr("transform", "rotate(-90)")
+    .attr("x", -1 * margin.left)
+    .attr("y", margin.right)
+    .style("font-size", "1.3em")
+    .text("Months");
+
+translateAxis.x = margin.left * 2;
+translateAxis.y = Math.abs(legendHeight - height);
+const legend = chart.append("g")
+    .attr("id", "legend")
+    .attr("transform", `translate(${translateAxis.x},${translateAxis.y})`)
+    .style("font-size", ".7em");
+
 d3.json(url).then(response => {
     const data = response.monthlyVariance;
     const years = d3.set(data.map(item => item.year)).values();
@@ -70,7 +93,7 @@ d3.json(url).then(response => {
         .range([0, width - margin.left - margin.right]);
     const scaleY = d3.scaleBand()
         .domain(months.reverse())
-        .range([height - margin.top - margin.bottom, 0]);
+        .range([height - margin.top - margin.bottom - legendHeight, 0]);
 
 
     const xAxis = d3.axisBottom()
@@ -80,10 +103,10 @@ d3.json(url).then(response => {
 
     d3.select("#desc").text(`${minYear} - ${maxYear} (base temperature ${baseTemp}°C)`);
 
-    const translateAxis = height - margin.top - margin.bottom;
+    translateAxis.x = height - margin.top - margin.bottom - legendHeight;
     chartPlot.append("g")
         .attr("id", "x-axis")
-        .attr("transform", `translate(0,${translateAxis})`)
+        .attr("transform", `translate(0,${translateAxis.x})`)
         .call(xAxis.ticks(20, "d"));
 
     chartPlot.append("g")
@@ -95,10 +118,13 @@ d3.json(url).then(response => {
         .enter()
         .append("rect")
         .attr("class", "cell")
+        .attr("data-month", d => d.months)
+        .attr("data-year", d => d.year)
+        .attr("data-temp", d => calcTemp(d.variance) )
         .attr("x", d => scaleX(d.year))
         .attr("y", d => scaleY(convertMonth(d.month)))
         .attr("width", (width - margin.left - margin.right) / years.length)
-        .attr("height", (height - margin.top - margin.bottom) /12)
+        .attr("height", (height - margin.top - margin.bottom - legendHeight) /12)
         .attr("fill", d => colorScale(d.variance))
         .on("mouseover", d => {
             tooltip.transition()
@@ -114,12 +140,35 @@ d3.json(url).then(response => {
                 .duration(250)
                 .style("opacity", 0)
         });
+    const legendItems = () => {
+        let valueForColor = Math.round(minVal * 10) / 10; //value for elem from myColor Array
+        let itemsLegend = [];
+        const step = Math.round((maxVal + Math.abs(minVal)) / myColor.length * 10) / 10 ;
 
-    chart.append("text")
-        .attr("text-anchor", "end")
-        .attr("transform", "rotate(-90)")
-        .attr("x", -1 * margin.left)
-        .attr("y", margin.right)
-        .style("font-size", "1.3em")
-        .text("Months");
+        while (valueForColor <= Math.round(maxVal * 10) / 10) {
+            itemsLegend.push(valueForColor);
+            valueForColor = Math.round((valueForColor + step) * 10) / 10;
+        }
+        return itemsLegend;
+    }
+
+    const legendItemWidth = legendWidth / 11;
+    legend.selectAll("rect")
+        .data(myColor)
+        .enter()
+        .append("rect")
+        .attr("x", (d, i) => legendItemWidth * i)
+        .attr("y", 0)
+        .attr("width", legendItemWidth)
+        .attr("height", legendItemWidth / 2)
+        .attr("fill", d => d)
+        .attr("stroke", "#212F3C");
+
+    legend.selectAll("text")
+        .data(legendItems)
+        .enter()
+        .append("text")
+        .attr("x", (d, i) => legendItemWidth * i)
+        .attr("y", legendHeight - legendItemWidth / 1.8)
+        .text(d => d);
 });
